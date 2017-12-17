af7986ac9bb2bb654cb28e9bea6287be3a2e978f
huker <huaolie@sina.com>
2017-12-17 14:03:16 +0800

    lesson link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
+    <div>
+        <Link href="/about">
+            <button>GO TO ABOUT PAGE</button>
+        </Link>
+        <p>Hello Next.js</p>
+    </div>
 )
 
 export default Index
